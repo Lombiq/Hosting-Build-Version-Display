13e86f691b2eb3211955ef1ae6e0d928748c2637
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-01-05 15:42:44 +0100

    Adding UI test

--- Lombiq.Hosting.BuildVersionDisplay.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Hosting.BuildVersionDisplay.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,4 +1,8 @@
+using Atata;
+using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using Shouldly;
 using System.Threading.Tasks;
 
 namespace Lombiq.Hosting.BuildVersionDisplay.Tests.UI.Extensions;
@@ -8,5 +12,16 @@ public static class TestCaseUITestContextExtensions
     /// <summary>
     /// Tests the Lombiq Hosting - Build Version Display for Orchard Core feature.
     /// </summary>
-    public static Task TestBuildVersionDisplayAsync(this UITestContext context) => Task.CompletedTask;
+    public static async Task TestBuildVersionDisplayAsync(this UITestContext context)
+    {
+        await context.SignInDirectlyAndGoToDashboardAsync();
+
+        var container = context.Get(By.Id("lombiq-hosting-build-version-display-build-version"));
+        var listItems = container.GetAll(By.TagName("li"));
+
+        var orchardVersion = typeof(OrchardCore.IOrchardHelper).Assembly.GetName().Version.ToString();
+        listItems.ShouldContain(element => element.Text == "Orchard Core version: " + orchardVersion);
+        listItems.ShouldContain(element => element.Text.Contains("App build version"));
+        listItems.ShouldContain(element => element.Text.Contains("Build link"));
+    }
 }

92c1036c2c943767bfbe306ce012fcde6c7885bc
Sára El-Saig <sara.el-saig@lombiq.com>
2024-09-24 11:05:39 +0200

    Seal INavigationProvider, IAsyncResultFilter, IPermissionProvider, StartupBase, IRecipeStepHandler, DisplayDriver, Controller classes.

--- Lombiq.Hosting.BuildVersionDisplay/Startup.cs
+++ Lombiq.Hosting.BuildVersionDisplay/Startup.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules;
 
 namespace Lombiq.Hosting.BuildVersionDisplay;
 
-public class Startup : StartupBase
+public sealed class Startup : StartupBase
 {
     public override void ConfigureServices(IServiceCollection services) =>
         services.Configure<MvcOptions>((options) => options.Filters.Add(typeof(BuildVersionDisplayInjectingFilter)));
